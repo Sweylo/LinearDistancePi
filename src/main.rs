f97109110f52f243a9c437882a68a002b16f95b6
Logan Nichols <logan@sweylo.net>
2025-09-10 18:10:46 -0400

    implement my own custom sqrt algorithm

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,11 @@
 
 use std;
 
+use malachite::Integer;
 use malachite_base::num::arithmetic::traits::Pow;
-// use malachite_base::num::arithmetic::traits::Sqrt;
-use malachite_base::num::arithmetic::traits::FloorSqrt;
 use malachite_base::num::conversion::string::options::ToSciOptions;
 use malachite_base::num::conversion::traits::ToSci;
 use malachite::rational::Rational;
-use malachite::natural::Natural;
-// use malachite::natural::arithmetic::sqrt::FloorSqrt;
 
 use clap::Parser;
 
@@ -27,13 +24,56 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   // parse cli args
   let args = Args::parse();
 
-  println!("Running with args: {:#?}\n", args);
+  println!("\nRunning with args: {:#?}\n", args);
 
-	let mut options = ToSciOptions::default();
+  let numerator = Integer::from(args.iter);
+  let denominator = Integer::from(1u64);
+
+  let now = std::time::Instant::now();
+
+  let sqrt = sqrt(&Rational::from_integers(numerator, denominator), args.prec);
+
+  let mut options = ToSciOptions::default();
 	options.set_precision(args.prec);
-	println!("{}", Rational::from(2).pow(3i64).to_sci_with_options(options));
-  println!("{}", Natural::from(2u64).floor_sqrt().to_sci_with_options(options));
+
+  println!("{}", sqrt.to_sci_with_options(options));
+
+  println!("\nTime elapsed: {:.2?}\n", now.elapsed());
 
   Ok(())
 
+}
+
+fn sqrt(x: &Rational, prec: u64) -> Rational {
+
+  let one = Rational::from(1u64);
+  let mut res = Rational::from(1u64);
+  let mut req = x.clone();
+
+  for _ in 0..prec {
+    let mut i = Rational::from(0u64);
+    loop {
+      let check = &res + &i;
+      let term = check.pow(2u64);
+      // println!("i = {}", i);
+      // println!("check = {}", check);
+      // println!("term = {}", term);
+      // println!("req = {}", req);
+      // println!("res = {}", res);
+      // println!();
+      if term > req {
+        i -= one.clone();
+        break;
+      }
+      i += one.clone();
+      // std::thread::sleep(std::time::Duration::from_millis(1000));
+    }
+    res = Rational::from(10u64) * (res + i);
+    req *= Rational::from(100u64);
+    // println!("res = {}", res);
+    // println!("req = {}", req);
+    // println!("-------------------");
+  }
+
+  return res / Rational::from(10u64).pow(prec as u64);
 }
